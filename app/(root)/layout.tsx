418bed53c561afe543987639428be505f6fbeee0
Shravan Joshi <jakkampudi@Tejas-MacBook-Air.local>
2024-09-21 20:09:03 +0530

    rightsidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSideBar from "@/components/shared/leftsidebar/LeftSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
+import RightSideBar from "@/components/shared/rightsidebar/RightSideBar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="max-w-5xl: mx-auto w-full">{children}</div>
         </section>
-        rightsidebar
+        <RightSideBar />
       </div>
       Toaster
     </main>
---
+++ components/shared/rightsidebar/RightSideBar.tsx
@@ -0,0 +1,101 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../RenderTag";
+
+const topQuestions = [
+  {
+    _id: 1,
+    title:
+      "Would it be appropriate to point out an error in another paper during a referee report?",
+  },
+  {
+    _id: 2,
+    title: "How can an airconditioning machine exist?",
+  },
+  {
+    _id: 3,
+    title: "Interrogated every time crossing UK Border as citizen",
+  },
+  {
+    _id: 4,
+    title: "Low digit addition generator",
+  },
+  {
+    _id: 5,
+    title: "What is an example of 3 numbers that do not make up a vector?",
+  },
+];
+
+const popularTags = [
+  {
+    _id: 1,
+    name: "REACTJS",
+    totalQuestions: 50,
+  },
+  {
+    _id: 2,
+    name: "NEXTJS",
+    totalQuestions: 36,
+  },
+  {
+    _id: 3,
+    name: "REACT",
+    totalQuestions: 22,
+  },
+  {
+    _id: 4,
+    name: "POSTGRES",
+    totalQuestions: 15,
+  },
+  {
+    _id: 5,
+    name: "PERFORMANCE",
+    totalQuestions: 7,
+  },
+];
+const RightSideBar = () => {
+  return (
+    <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-fit flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden xl:w-[330px] custom-scrollbar">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 w-full flex flex-col gap-[30px]">
+          {topQuestions.map((question) => (
+            <Link
+              key={question._id}
+              href={`/questions/${question._id}`}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((item) => (
+            <RenderTag
+              key={item._id}
+              _id={item._id}
+              name={item.name}
+              totalQuestions={item.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSideBar;
