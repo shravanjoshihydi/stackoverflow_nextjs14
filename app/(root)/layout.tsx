1af41ba5f137e02ac21769dfc88814b251db8bfd
Shravan Joshi <jakkampudi@Tejas-MacBook-Air.local>
2024-09-21 18:45:56 +0530

    left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSideBar from "@/components/shared/leftsidebar/LeftSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -6,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        leftsidebar
+        <LeftSideBar />
         <section className="min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="max-w-5xl: mx-auto w-full">{children}</div>
         </section>
